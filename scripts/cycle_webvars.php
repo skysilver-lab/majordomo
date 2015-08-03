5d1e4fbfcc219d1eb3dd7267670a8b6c8a1a9d22
skysilver <skysilver.da@gmail.com>
2015-08-03 16:41:47 +0300

    add code style support

--- scripts/cycle_webvars.php
+++ scripts/cycle_webvars.php
@@ -9,7 +9,7 @@ include_once("./lib/threads.php");
 set_time_limit(0);
 
 // connecting to database
-$db = new mysql(DB_HOST, '', DB_USER, DB_PASSWORD, DB_NAME); 
+$db = new mysql(DB_HOST, '', DB_USER, DB_PASSWORD, DB_NAME);
 
 include_once("./load_settings.php");
 include_once(DIR_MODULES . "control_modules/control_modules.class.php");
@@ -20,32 +20,28 @@ include_once(DIR_MODULES . 'webvars/webvars.class.php');
 
 $webvars = new webvars();
 
-$checked_time=0;
+$checked_time = 0;
 
-echo date("Y-m-d H:i:s") . " running " . basename(__FILE__) . "\n";
-while(1) 
+echo date("H:i:s") . " running " . basename(__FILE__) . PHP_EOL;
+
+while (1)
 {
-   if (time()-$checked_time>10) {
-    $checked_time=time();
-    setGlobal((str_replace('.php', '', basename(__FILE__))).'Run', time(), 1);
-    // check all web vars
-    $webvars->checkAllVars(); 
+   if (time() - $checked_time > 10)
+   {
+      $checked_time = time();
+      setGlobal((str_replace('.php', '', basename(__FILE__))) . 'Run', time(), 1);
+      
+      // check all web vars
+      $webvars->checkAllVars();
    }
 
-   if (file_exists('./reboot') || $_GET['onetime']) 
+   if (file_exists('./reboot') || $_GET['onetime'])
    {
       $db->Disconnect();
-      echo date("Y-m-d H:i:s ") . "Stopping by command REBOOT " . basename(__FILE__) . "\n";
       exit;
    }
 
    sleep(1);
 }
 
-$db->Disconnect(); 
-
-echo date("Y-m-d H:i:s ") . "Unexpected stopping " . basename(__FILE__) . "\n";
-
 DebMes("Unexpected close of cycle: " . basename(__FILE__));
-
-?>
