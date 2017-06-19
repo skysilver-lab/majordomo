2fe27199eac0f805b0e5ad8c2e05e336f2c0fb13
skysilver <skysilver.da@gmail.com>
2017-06-20 00:15:57 +0300

    Update SControllers_switch.php

--- modules/devices/SControllers_switch.php
+++ modules/devices/SControllers_switch.php
@@ -2,7 +2,9 @@
 
 $currentStatus=$this->getProperty('status');
 if ($currentStatus) {
- $this->callmethodSafe('turnOff');
+ //$this->callmethodSafe('turnOff');
+ $this->callmethod('turnOff');
 } else {
- $this->callmethodSafe('turnOn');
+ //$this->callmethodSafe('turnOn');
+ $this->callmethod('turnOn');
 }
