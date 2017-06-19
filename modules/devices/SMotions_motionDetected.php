1836cffeb1742452ee0f7201d2071acbb84c3191
skysilver <skysilver.da@gmail.com>
2017-06-20 00:15:28 +0300

    Update SMotions_motionDetected.php

--- modules/devices/SMotions_motionDetected.php
+++ modules/devices/SMotions_motionDetected.php
@@ -19,13 +19,15 @@
  $this->callMethod('logicAction');
  $linked_room=$this->getProperty('linkedRoom');
  if ($nobodysHome) {
-  callMethodSafe('NobodyHomeMode.deactivate', array('sensor'=>$ot, 'room'=>$linked_room));
+  //callMethodSafe('NobodyHomeMode.deactivate', array('sensor'=>$ot, 'room'=>$linked_room));
+  callMethod('NobodyHomeMode.deactivate', array('sensor'=>$ot, 'room'=>$linked_room));
  }
  ClearTimeOut("nobodyHome"); 
  SetTimeOut("nobodyHome","callMethod('NobodyHomeMode.activate');", 1*60*60);
 
  if ($linked_room) {
-  callMethodSafe($linked_room.'.onActivity', array('sensor'=>$ot));
+  //callMethodSafe($linked_room.'.onActivity', array('sensor'=>$ot));
+  callMethod($linked_room.'.onActivity', array('sensor'=>$ot));
  }
 
 /*
